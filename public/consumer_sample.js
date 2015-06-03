342318ff76d7fc2a7cc34aa7757abe0a49896d26
timatej <tsakharchuk@exadel.com>
2015-06-03 14:17:55 +0300

    sample file

---
+++ public/consumer_sample.js
@@ -0,0 +1,27 @@
+var consumer = {
+    stories: {
+        //generated_list
+
+    },
+    getTaskUrl: function (taskId) {
+        return 'https://abcjira.disney.com/browse/' + taskId;
+    },
+    alerts: {
+        noBlockerPerson: 'You should specify who is the person you are waiting for to continue work. Please contact Tim if you have any questions.',
+        noTicketNumber: 'Your activity should be related to Story or Defect. Please contact Tim for a new one if needed.'
+    },
+    //we cannot use simple array here, because AngularJS work incorrectly with it.
+    team: {
+        'Tim Sakharchuk': {},
+        'Alex Shoferyuk': {},
+        'Maksym Ranchev': {}
+    },
+    locations: {
+        'US':{},
+        'EMEA':{}
+    },
+    email: {
+        'to': 'tsakharchuk@exadel.com',
+        'subject': 'AppleTV Daily Status Report'
+    }
+};
