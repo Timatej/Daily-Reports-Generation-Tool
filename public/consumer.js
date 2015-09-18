e831b546d500fcf2c4de5f541565e6db8d623af1
Tim Sakharchuk <timatej@Tims-MacBook-Pro.local>
2015-09-18 11:57:58 +0300

    Jira connector - generation 2

--- public/consumer.js
+++ public/consumer.js
@@ -1,28 +1,30 @@
 var consumer = {
     stories: {
-        // from 1 to 10 are special placeholder ids.
-        // for such action items there will no link be displayed. Just story text
-        1: 'Non-ScrumA Activities',
-        2: 'Regression Testing',
-        3322: 'Test Story',
-        3131: 'Test Story 2'
+        
 
     },
     getTaskUrl: function (taskId) {
-        return 'URL to task nubmer ' + taskId;
+        return 'https://abcjira.disney.com/browse/' + taskId;
     },
     alerts: {
-        noBlockerPerson: 'Alert text for the case when user forgot to mention the person who blocks his activities',
-        noTicketNumber: 'Alert text for the case when action item is not specified.'
+        noBlockerPerson: 'You should specify who is the person you are waiting for to continue work. Please contact Tim if you have any questions.',
+        noTicketNumber: 'Your activity should be related to Story or Defect. Please contact Tim for a new one if needed.'
     },
-    //we cannot use simple array here, because AngularJS works incorrectly with it.
+    //we cannot use simple array here, because AngularJS work incorrectly with it.
     team: {
-        'Developer 1': {},
-        'Developer 2': {}
+        'Tim Sakharchuk': {},
+        'Alex Shoferyuk': {},
+        'Maksym Ranchev': {},
+        'Eduard Trayan': {},
+        'Pavel Yaschenko': {},
+        'Igor Ivanuto': {}
     },
     locations: {
-        'Location1':{},
-        'Location2':{},
-        'Location3':{}
+        'US':{},
+        'EMEA':{}
+    },
+    email: {
+        'to': 'tsakharchuk@exadel.com',
+        'subject': 'AppleTV Daily Status Report'
     }
-}
+};
